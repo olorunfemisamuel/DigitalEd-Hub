3d65186b78701a2b8e5b162b85d22073b0482900
Olorunfemi Samuel <forfemi@gmail.com>
2026-03-09 13:57:44 +0100

    fix: used process.env in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,15 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/supabase',
   ],
-
   css: ['~/assets/css/main.css'],
-
   supabase: {
     redirect: false,
-    url: 'https://ttqysorngmhbnbjtiwno.supabase.co',        // hardcoded temporarily
-    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0cXlzb3JuZ21oYm5ianRpd25vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5OTYyNTQsImV4cCI6MjA4ODU3MjI1NH0.60CLeCfh7s2iWLTsLWG4sTMOsrQXCorgPc8TKOK1wp8',  // hardcoded temporarily
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_ANON_KEY,
   },
-
   runtimeConfig: {
     supabaseServiceKey: process.env.SUPABASE_SERVICE_KEY,
     paystackSecretKey: process.env.PAYSTACK_SECRET_KEY,
